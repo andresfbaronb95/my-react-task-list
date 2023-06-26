5260db08d681e136961f2e19029e33055a549a56
Andres Felipe Baron <pipe5477@gmail.com>
2023-06-26 02:09:48 -0500

    Se agrego estilo a la app y se crearon hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './assets/components/Header';
 import TaskList from './assets/components/TaskList';
 
 function App() {
-  let myList = ["Lavar la ropa", "Sacar la basura"];
+  const [myList, setMyList] = useState(() => {
+    const storedList = localStorage.getItem('taskList');
+    return storedList ? JSON.parse(storedList) : [];
+  });
+
+  useEffect(() => {
+    const storedList = localStorage.getItem('taskList');
+    if (storedList) {
+      setMyList(JSON.parse(storedList));
+    }
+  }, []);
+
+  const addTask = () => {
+    const taskName = window.prompt('Ingrese el nombre de la tarea');
+    if (taskName) {
+      const newTask = {
+        description: taskName.trim(),
+        completed: false
+      };
+      setMyList([...myList, newTask]);
+      localStorage.setItem('taskList', JSON.stringify([...myList, newTask]));
+    }
+  };
+
   return (
-    <>
+    <div className="app">
       <Header />
       <TaskList list={myList} />
-    </>
-  )
+      <button onClick={addTask}>Agregar Tarea</button>
+    </div>
+  );
 }
 
-export default App
- 
+export default App;
